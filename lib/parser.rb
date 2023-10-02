6e824f91bd501ebe1433d35fad7487a3eb535bd8
André Kupkovski <kupkovski@gmail.com>
2023-10-01 21:29:04 -0300

    Refactoring to remove complexity

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,19 +4,17 @@
 class Parser
   require 'pry'
   require 'pry-nav'
+  require 'json'
   require_relative 'game'
   require_relative 'player'
 
   def initialize(quake_log_path:)
     @quake_log_path = quake_log_path
-    @current_game = nil
     @games = []
   end
 
   # rubocop:disable Metrics/MethodLength:
   # rubocop:disable Metrics/AbcSize:
-  # rubocop:disable Metrics/CyclomaticComplexity:
-  # rubocop:disable Metrics/PerceivedComplexity::
   def parse
     raise ArgumentError, 'quake_log_path should not be empty' if quake_log_path.nil?
     raise ArgumentError, 'quake_log_path should not be empty' if quake_log_path.empty?
@@ -24,31 +22,36 @@ class Parser
     current_game = nil
 
     File.readlines(quake_log_path, chomp: true).each do |line|
-      if line.match(/InitGame:/)
-        current_game = Game.new(number: games.size)
-        games << current_game
-      else
-        current_game = games.last
-      end
-
+      current_game = get_current_game(line)
       next unless line.match(/Kill:/)
 
-      parsed_line = line.match(/(<world>|[\w|\s]+)\skilled\s([\w|\s]+)by\s([\w|\s]+)/)
+      parsed_line = parse_line(line)
       next unless parsed_line
 
-      killer_name = parsed_line[1]&.strip
-      victim_name = parsed_line[2]&.strip
-
-      current_game.parse_kill(killer_name, victim_name)
+      killer_name, victim_name = players_names(parsed_line)
+      current_game.build_kill(killer_name, victim_name)
     end
-    games_report(games)
+    JSON(games_report(games))
   end
   # rubocop:enable Metrics/MethodLength:
   # rubocop:enable Metrics/AbcSize:
-  # rubocop:enable Metrics/CyclomaticComplexity:
-  # rubocop:enable Metrics/PerceivedComplexity::
 
   private
+
+  def players_names(parsed_line)
+    killer_name = parsed_line[1]&.strip
+    victim_name = parsed_line[2]&.strip
+    [killer_name, victim_name]
+  end
+
+  def parse_line(line)
+    line.match(/(<world>|[\w|\s]+)\skilled\s([\w|\s]+)by\s([\w|\s]+)/)
+  end
+
+  def get_current_game(line)
+    games << Game.new(number: games.size) if line.match(/InitGame:/)
+    games.last
+  end
 
   def games_report(games)
     games.map(&:report)
